a8457ef5416bc97a2c4c2675e3cff6a62cdcf88b
Nuozhihui <1285574579@qq.com>
2022-03-17 21:31:21 +0800

    v0.99
    远程控制

---
+++ RF24_89C52_RX/usar/main.c
@@ -0,0 +1,107 @@
+
+#include "main.h"
+
+const char *content = "yunguokeji";
+u8   UART_BUFFER[64]={0},RF24L01RxBuffer[64]={0};
+
+unsigned char pwm_motor_val =255;//左右电机占空比值 取值范围0-170，0最快
+unsigned char pwm_t;//周期
+
+sbit BEEP=P2^3;
+sbit PWMA = P0^0;//
+sbit PWMB = P0^5;//
+
+sbit AIN1 = P0^1;//
+sbit AIN2 = P0^2;//
+sbit BIN1 = P0^3;
+sbit BIN2 = P0^4;//
+//定时器0中断
+void timer0() interrupt 1
+{
+	pwm_t++;
+	if(pwm_t == 255)
+	{
+		AIN1 = 0;
+		BIN1 = 0;
+	}
+	if(pwm_motor_val == pwm_t)
+	{	
+		AIN1 = 1;			
+		BIN1 = 1;	
+	}
+}
+
+void main()
+{
+	u8 j;
+	u32 num=0;
+	OLED_Init();			   //初始化OLED  
+    OLED_Clear();
+	
+	drv_spi_init( );           //SPI初始化
+	
+	
+	NRF24L01_Gpio_Init( );    //RF24L01引脚初始化
+	
+
+	NRF24L01_check( );	 	  //检测nRF24L01
+	RF24L01_Init( );
+	RF24L01_Set_Mode( MODE_RX );		//接收模式
+	OLED_ShowCHinese(0,2,0);     //接
+
+	
+	
+	
+	TMOD |= 0x02;//8位自动重装模块
+	TH0 = 220;
+	TL0 = 220;//11.0592M晶振下占空比最大比值是256,输出100HZ
+	TR0 = 1;//启动定时器0
+	ET0 = 1;//允许定时器0中断
+	EA	= 1;//总中断允许
+	
+	AIN2 = 1;
+	BIN2 = 1;
+	
+	PWMA=0;
+	PWMB=0;
+
+	//********************************接收*************************************//
+	while(1)
+	{
+//		
+		 j = NRF24L01_RxPacket( RF24L01RxBuffer );		//接收字节
+	
+		if( 0 != j )
+		{
+//			OLED_Clear();
+//			OLED_ShowString(0,num,RF24L01RxBuffer,8);		
+//			num++;
+//	    if(num==8)  num=0;
+		
+			if( RF24L01RxBuffer[1]==1)
+			{
+				BEEP=0;
+				
+				pwm_motor_val=200;
+			}
+
+			if( RF24L01RxBuffer[1]==2)
+			{
+				BEEP=1;
+					pwm_motor_val=255;
+			}
+			if( RF24L01RxBuffer[1]==3)
+			{
+				BEEP=0;
+				pwm_motor_val=200;
+			}
+			if( RF24L01RxBuffer[1]==4)
+			{
+				BEEP=1;
+				pwm_motor_val=255;
+			}
+	}
+//	
+	
+	}
+}
---
+++ RF24_89C52_TX/usar/main.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+
+sbit KEY1=P2^0;
+sbit KEY2=P2^1;
+sbit KEY3=P2^3;
+sbit KEY4=P2^2;
+
+const char *content = "yunguokeji";
+	unsigned char TxBuf[20]={0};	 // 
+u8   UART_BUFFER[64]={0},RF24L01RxBuffer[64]={0};
+void main()
+{
+	u32 num=0;
+	OLED_Init();			   //初始化OLED  
+    OLED_Clear();
+	
+	drv_spi_init( );           //SPI初始化
+	
+	
+	NRF24L01_Gpio_Init( );    //RF24L01引脚初始化
+	
+
+	NRF24L01_check( );	 	  //检测nRF24L01
+	RF24L01_Init( );
+	RF24L01_Set_Mode( MODE_TX );		//发送模式
+
+	
+	//****************发送********************************************//
+	while(1)
+	{
+		 RF24L01_Set_Mode( MODE_TX );		//发送模式
+		 
+//		 NRF24L01_TxPacket( UART_BUFFER, 10 );		//模式1发送固定字符,1S一包
+//		 num++;
+//		 OLED_ShowNum(90,3,num,3,16);
+//		 delay_ms( 1000 );
+		
+		if( KEY1==0)
+		{
+			 delay_ms( 5);
+			 if( KEY1==0)
+				{
+					 TxBuf[1] = 1 ;
+					 NRF24L01_TxPacket(TxBuf, 2 );		//模式1发送固定字符,1S一包
+					OLED_Clear();  
+					OLED_ShowString(10,10,"advance",4);
+				}
+		}
+		
+		if( KEY2==0)
+		{
+			 delay_ms( 5);
+			 if( KEY2==0)
+				{
+					 TxBuf[1] = 2 ;
+					 NRF24L01_TxPacket(TxBuf, 2 );		//模式1发送固定字符,1S一包
+					OLED_Clear();  
+					OLED_ShowString(10,10,"Stop",4);
+					
+				}
+		}
+		
+		if( KEY3==0)
+		{
+			 delay_ms( 5);
+			 if( KEY3==0)
+				{
+					 TxBuf[1] = 3 ;
+					 NRF24L01_TxPacket(TxBuf, 2 );		//模式1发送固定字符,1S一包
+					OLED_Clear();  
+					OLED_ShowString(10,10,"left",4);
+				}
+		}
+		
+		if( KEY4==0)
+		{
+			 delay_ms( 5);
+			 if( KEY4==0)
+				{
+					 TxBuf[1] = 4 ;
+					 NRF24L01_TxPacket(TxBuf, 2 );		//模式1发送固定字符,1S一包
+					OLED_Clear();  
+					OLED_ShowString(10,10,"right",4);
+				}
+		}
+		TxBuf[1] = 0x00;
+		TxBuf[2] = 0x00;
+	}
+}
